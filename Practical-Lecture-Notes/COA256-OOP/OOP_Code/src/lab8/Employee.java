e0b173877aae2c74895e05b4a775a938e47f087b
athelite <athelite@localhost>
2026-04-14 13:53:07 +0100

    updating lab 8

--- Practical-Lecture-Notes/COA256-OOP/OOP_Code/src/lab8/Employee.java
+++ Practical-Lecture-Notes/COA256-OOP/OOP_Code/src/lab8/Employee.java
@@ -15,6 +15,46 @@ public class Employee {
 		this.dateOFContract = dateOFContract;
 		this.salary = salary;
 	}
+	
+	//getters and setters
+	public int getEmployeeId() {
+		return EMPLOYEE_ID;
+	}
+	
+	public String getName() {
+		return name;
+	}
+	
+	public void setName(String name) {
+		this.name = name;
+	}
+	
+	public LocalDate getDateOFContract() {
+		return dateOFContract;
+	}
+	
+	public void setDateOFContract(LocalDate dateOFContract) {
+		this.dateOFContract = dateOFContract;
+	}
+	
+	//set date being passed as a string in the format of "DD-MM-YYYY"
+	public void setDateOFContract(String date) {
+		String[] dateParts = date.split("-");
+		int day = Integer.parseInt(dateParts[0]);
+		int month = Integer.parseInt(dateParts[1]);
+		int year = Integer.parseInt(dateParts[2]);
+		this.dateOFContract = LocalDate.of(year, month, day);
+	}
+	
+	public int getSalary() {
+		return salary;
+	}
+	
+	public void setSalary(int salary) {
+		this.salary = salary;
+	}
+	
+	
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
